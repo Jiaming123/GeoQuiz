85489ed6acbd07a06174a452a1ade9d51c23fee8
Jiaming123 <jlkhf@mail.umkc.edu>
2016-02-22 18:25:10 -0600

    1

--- app/src/main/java/jiaming/geoquiz/MainActivity.java
+++ app/src/main/java/jiaming/geoquiz/MainActivity.java
@@ -9,7 +9,8 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-
+//Testing
+//adding things more and more
 public class QuizActivity extends Activity {
 
     private Button mTrueButton;
